7031529d979aff6d1be862aec5f29b6267376712
lkasta <lkastabackup@gmail.com>
2023-05-09 14:33:33 -0300

    update js

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
 const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+var year = new Date().getFullYear()
+var month = new Date().getMonth()
 
 function render() {
     let output = ''
@@ -16,6 +18,90 @@ function render() {
 app.querySelector('months').innerHTML = render()
 app.querySelector('header span').innerHTML = new Date().getFullYear();
 
+const daysWeek = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb']
+function renderDaysWeek() {
+    let output = ''
+
+    for (let dayWeek of daysWeek) {
+        output = output + '<div class="dayWeek">' + dayWeek + '</div>'
+    }
+
+    return output
+}
+app2.querySelector('daysWeek').innerHTML = renderDaysWeek()
+
+function getDaysOfMonth(year, month) {
+    const daysInMonth = new Date(year, month, 0).getDate();
+    const firstDayOfMonth = new Date(year, month - 1, 1).getDay();
+    const days = [];
+
+    // Preenche os dias do mês anterior
+    const daysInPreviousMonth = new Date(year, month - 1, 0).getDate();
+    const startDay = daysInPreviousMonth - firstDayOfMonth + 1;
+    for (let i = startDay; i <= daysInPreviousMonth; i++) {
+        days.push({
+            day: i,
+            isCurrentMonth: false
+        });
+    }
+
+    // Preenche os dias do mês atual
+    for (let i = 1; i <= daysInMonth; i++) {
+        days.push({
+            day: i,
+            isCurrentMonth: true
+        });
+    }
+
+    // Preenche os dias do próximo mês
+    const lastDayOfMonth = new Date(year, month - 1, daysInMonth).getDay();
+    const daysToAdd = 6 - lastDayOfMonth;
+    for (let i = 1; i <= daysToAdd; i++) {
+        days.push({
+            day: i,
+            isCurrentMonth: false
+        });
+    }
+
+    return days;
+}
+
+function getCurrentDay() {
+    const today = new Date();
+    const day = today.getDate();
+    return day;
+}
+
+function renderDays() {
+    const currentDay = getCurrentDay();
+
+    const days = getDaysOfMonth(2023, 5)
+    let output = ''
+
+    for (let day of days) {
+        if (day.isCurrentMonth == false) {
+            output = output + '<div class="day false">' + day.day + '</div>'
+        } else if (day.day == currentDay) {
+            output = output + '<div class="day active">' + day.day + '</div>'
+        } else {
+            output = output + '<div class="day">' + day.day + '</div>'
+        }
+
+    }
+
+    return output
+}
+app2.querySelector('days').innerHTML = renderDays()
+
+function renderDate(mes = 0, ano = 0) {    
+    const date = months[month += (mes)] + ' ' + (year += ano)
+    return date
+}
+
+app2.querySelector('span').innerHTML = renderDate();
+
+// Atualiza Ano
+
 document.querySelector('#btnLeft').addEventListener("click", function () {
     const currentYear = parseInt(app.querySelector('header span').innerHTML);
     app.querySelector('header span').innerHTML = currentYear - 1;
@@ -26,6 +112,20 @@ document.querySelector('#btnRight').addEventListener("click", function () {
     app.querySelector('header span').innerHTML = currentYear + 1;
 });
 
+// Atualiza Mes e Ano
+
+document.querySelector('#btnLeftm').addEventListener("click", function () {
+    app2.querySelector('span').innerHTML = renderDate(-1, 0);
+});
+
+document.querySelector('#btnRightm').addEventListener("click", function () {
+    app2.querySelector('span').innerHTML = renderDate(1, 0);
+});
 
 
-//onselectstart=”return false //Desabilita função de copiar
+//------------------------------------
+
+document.querySelector('#btnRight').addEventListener("click", function () {
+    const currentYear = parseInt(app.querySelector('header span').innerHTML);
+    app.querySelector('header span').innerHTML = currentYear + 1;
+});
